6e90dd90f280c5a2ea4a20dbb8b4e92135471aee
cc32d9 <cc32d9@gmail.com>
2019-07-19 20:48:08 +0000

    bugfix in  couchdb/add_track_contract.sh

--- couchdb/add_track_contract.sh
+++ couchdb/add_track_contract.sh
@@ -1,15 +1,17 @@
 . ./chkvars.sh
 
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 NEWORK ACCOUNT TYPE" 1>&2;
+if [ $# -ne 5 ]; then
+    echo "Usage: $0 NEWORK ACCOUNT TYPE TRACKTABLES TRACKTX" 1>&2;
     exit 1;
 fi
 
 NETWORK=$1
 ACC=$2
 TYPE=$3
+TRACKTABLES=$4
+TRACKTX=$5
 
 
 cbc create contract:${NETWORK}:${ACC} -V \
-'{"type":"contract", "network":"'${NETWORK}'", "account_name":"'${ACC}'", "contract_type":"'${TYPE}'", "track_tables":"true", "track_tx":"true"}' \
+'{"type":"contract", "network":"'${NETWORK}'", "account_name":"'${ACC}'", "contract_type":"'${TYPE}'", "track_tables":"'${TRACKTABLES}'", "track_tx":"'${TRACKTX}'"}' \
 -M upsert -u $COUCH_USER -P $COUCH_PW -U couchbase://localhost/$COUCH_BUCKET

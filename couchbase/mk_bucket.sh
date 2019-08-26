d6bd13589d432b069ff3dc0eaf97a1a1f90e552b
cc32d9 <cc32d9@gmail.com>
2019-08-26 12:20:10 +0000

    fix in mk_bucket.sh

--- couchbase/mk_bucket.sh
+++ couchbase/mk_bucket.sh
@@ -1,4 +1,4 @@
 . ./chkvars.sh
 
-couchbase-cli bucket-create --username $COUCH_USER --password $COUCH_PW  --cluster couchbase://localhost --bucket $COUCH_BUCKET --bucket-eviction-policy fullEviction --bucket-type couchbase
+couchbase-cli bucket-create --username $COUCH_USER --password $COUCH_PW  --cluster couchbase://localhost --bucket $COUCH_BUCKET --bucket-eviction-policy fullEviction --bucket-type couchbase --bucket-ramsizei 100
 
